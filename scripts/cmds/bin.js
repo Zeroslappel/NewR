6dbbc9989e364eec0bbb1fa2cd43dfcf958699de
Zeroslappel <132753954+Zeroslappel@users.noreply.github.com>
2024-03-25 18:48:45 +0545

    Update bin.js

--- scripts/cmds/bin.js
+++ scripts/cmds/bin.js
@@ -1,31 +1,32 @@
+const PastebinAPI = require('pastebin-js');
 const fs = require('fs');
 const path = require('path');
-const axios = require('axios');
 
 module.exports = {
   config: {
     name: "bin",
-    aliases: ["pastebin"],
     version: "1.0",
-    author: "rehat--",
+    author: "",
     countDown: 5,
     role: 2,
-    longDescription: {
-      en: "This command allows you to upload files in turtle-bin"
+    shortDescription: {
+      en: "Upload files to pastebin and sends link"
     },
-    category: "owner",
-    guide: {en: "To use this command, type !bin <name>"}
+    longDescription: {
+      en: "This command allows you to upload files to pastebin and sends the link to the file."
+    },
+    category: "admin",
+    guide: {
+      en: "To use this command, type !pastebin <filename>. The file must be located in the 'cmds' folder."
+    }
   },
 
-  onStart: async function ({ api, event, args, content }) {
-    const text = args.slice(1).join(" ");
-    const permission = ["100092010460002"];
-    if (!permission.includes(event.senderID)) {
-      return api.sendMessage("You don't have enough permission to use this command. Only Ohio03 can do it.", event.threadID, event.messageID);
-    }
-    if (!args[0]) {
-      return api.sendMessage('Please learn how to use !bin <name>', event.threadID);
-    }
+  onStart: async function({ api, event, args }) {
+    const pastebin = new PastebinAPI({
+      api_dev_key: 'LFhKGk5aRuRBII5zKZbbEpQjZzboWDp9',
+      api_user_key: 'LFhKGk5aRuRBII5zKZbbEpQjZzboWDp9',
+    });
+
     const fileName = args[0];
     const filePathWithoutExtension = path.join(__dirname, '..', 'cmds', fileName);
     const filePathWithExtension = path.join(__dirname, '..', 'cmds', fileName + '.js');
@@ -35,24 +36,24 @@ module.exports = {
     }
 
     const filePath = fs.existsSync(filePathWithoutExtension) ? filePathWithoutExtension : filePathWithExtension;
-    fs.readFile(filePath, 'utf8', async (err, data) => { if (err) throw err;
 
-      const pasteData = {
-        name: fileName,
-        content: data,
-        key: "rehatXD",
-        burn: "false",
-        lock: "false",
-        encrypt: "false"
-      };
-      try {
-        const response = await axios.post("https://turtle-bin.onrender.com/upload", pasteData);
-        const rawPaste = response.data.url.replace("turtle-bin./onrender.com", "turtle-bin.onrender.com/raw");
-        api.sendMessage(`${rawPaste}`, event.threadID , event.messageID);
+    fs.readFile(filePath, 'utf8', async (err, data) => {
+      if (err) throw err;
 
-      } catch (error) {
-        console.error(error);
-      }
+      const paste = await pastebin
+        .createPaste({
+          text: data,
+          title: fileName,
+          format: null,
+          privacy: 1,
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+      const rawPaste = paste.replace("pastebin.com", "pastebin.com/raw");
+
+      api.sendMessage(`cmd install ${fileName}.js ${rawPaste}`, event.threadID);
     });
   },
 };
